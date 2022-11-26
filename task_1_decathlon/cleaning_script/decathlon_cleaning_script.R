84d745fbdeb787af7d9eb2269f53cb16f44c3a00
Fiona Carson <fionascarson@gmail.com>
2022-11-26 11:33:03 +0000

    updating decathlon markdown and prepping candy project

--- task_1_decathlon/cleaning_script/decathlon_cleaning_script.R
+++ task_1_decathlon/cleaning_script/decathlon_cleaning_script.R
@@ -4,22 +4,22 @@ library(janitor)
 decathlon <- read_rds("raw_data/decathlon.rds")
 
 # Cleaning column names
-
 decathlon <- janitor::clean_names(decathlon)
 
-
+# Correcting a typo
 decathlon <- rename(decathlon, "javelin" = "javeline")
 
+# Converting row names to a column
 decathlon <- rownames_to_column(decathlon, "athlete")
 
 # No NAs
 # No NULLs
 # all values in expected range
 
-#changing athletes names to all be lowercase
+# Changing athletes names to all be lowercase
 decathlon$athlete <- tolower(decathlon$athlete)
 
-# Tidying up competition names (bit pedantic but looks neater)
+# Tidying up competition names 
 decathlon <- decathlon %>% 
   mutate(competition = if_else(competition == "Decastar", "decastar", "olympics"))
 
