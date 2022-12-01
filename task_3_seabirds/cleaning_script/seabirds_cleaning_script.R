a2c39c0f6a38f7d134463df0c0620998ff6ab645
Fiona Carson <fionascarson@gmail.com>
2022-12-01 19:37:34 +0000

    Update seabirds_cleaning_script.R

--- task_3_seabirds/cleaning_script/seabirds_cleaning_script.R
+++ task_3_seabirds/cleaning_script/seabirds_cleaning_script.R
@@ -10,13 +10,13 @@ bird_codes <- read_excel("raw_data/seabirds.xls", sheet = "Bird data codes")
 bird <- janitor::clean_names(bird)
 ship <- janitor::clean_names(ship)
 
+# A lot of columns were removed from both the bird and ship datasets as they 
+# didn't provide useful information or weren't relevant for answering the questions.  
 
 bird <- bird %>% 
   rename(common_name = species_common_name_taxon_age_sex_plumage_phase, 
          scientific_name = species_scientific_name_taxon_age_sex_plumage_phase,
          accomp_num = nacc) %>% 
-# The columns below were removed as they didn't provide useful information or 
-# weren't relevant for answering the questions  
   select(-record, -wanplum, -plphase, -sex, -nfeed, -ocfeed, -nsow,
          -ocsow, -nsoice, -ocsoice, -ocsoshp, -ocinhd, -nflyp, -ocflyp, -nfoll, 
          -ocfol, -ocmoult, -ocnatfed, -ocacc)
@@ -30,9 +30,7 @@ ship <- ship %>%
 
 # Conducted a full join to ensure we weren't losing any data. We originally get 
 # one more record than expected. Upon investigation it looks like a typo has 
-# been made in the record_id for an albatross. The sequence of numbers were 
-# checked in both datasets and the order looks out in the bird dataset so this 
-# record will be fixed manually. 
+# been made in the record_id for an albatross in the bird dataset - this was fixed. 
 
 bird <- bird %>% 
 # Filtered to check there was only one record with this number before we change it. 
@@ -40,46 +38,44 @@ bird <- bird %>%
     mutate(record_id = replace(record_id, record_id == 1184009, 1104009))
 
 
-# Could use regex for this and remove the uppercase letters from the common_name
-# variable but this also remove other information, such as record 1087001 where 
+# Could use regex to remove the uppercase letters from the common_name
+# variable but this also removes other information, such as from record 1087001 where 
 # it says "NO BIRDS RECORDED".
 # Decided to remove exact strings as this can also be used on scientific column.
 
-fix_names <- function(col)
-bird %>% 
-  mutate(col = str_remove_all(common_name, "sensu lato")) %>% 
+
+bird <- bird %>% 
+  mutate(common_name = str_remove_all(common_name, "sensu lato")) %>% 
   mutate(common_name = str_remove_all(common_name, "AD")) %>% 
   mutate(common_name = str_remove_all(common_name, "SUBAD")) %>% 
   mutate(common_name = str_remove_all(common_name, "SUB")) %>% 
-    mutate(common_name = str_remove_all(common_name, "IMM")) %>% 
-  mutate(common_name = str_remove_all(common_name, "JUV")) 
+  mutate(common_name = str_remove_all(common_name, "IMM")) %>% 
+  mutate(common_name = str_remove_all(common_name, "JUV")) %>%
+  mutate(common_name = str_remove_all(common_name, "PL[1-5]")) 
 
 
-fix_names <- function(col){
-  bird %>% 
-    mutate(col = str_remove_all(col, "sensu lato")) %>% 
-    mutate(col = str_remove_all(col, "AD")) %>% 
-    mutate(col = str_remove_all(col, "SUBAD")) %>% 
-    mutate(col = str_remove_all(col, "SUB")) %>% 
-    mutate(col = str_remove_all(col, "IMM")) %>% 
-    mutate(col = str_remove_all(col, "JUV")) %>% 
-    mutate(col = str_remove_all(col, "[PL][1-5]"))
-}
+bird <- bird %>% 
+  mutate(scientific_name = str_remove_all(scientific_name, "sensu lato")) %>% 
+  mutate(scientific_name = str_remove_all(scientific_name, "AD")) %>% 
+  mutate(scientific_name = str_remove_all(scientific_name, "SUBAD")) %>% 
+  mutate(scientific_name = str_remove_all(scientific_name, "SUB")) %>% 
+  mutate(scientific_name = str_remove_all(scientific_name, "IMM")) %>% 
+  mutate(scientific_name = str_remove_all(scientific_name, "JUV")) %>%
+  mutate(scientific_name = str_remove_all(scientific_name, "PL[1-5]")) 
 
-
-bird2 <- fix_names("common_name")
-
-  
-# I'm not convinced regex is the best way to go here. Multiple lines of code to 
-# remove the actual offending expression might be better and could then be used for 
-bird3 <- bird %>% 
-  mutate(common_name = str_remove_all(common_name, "sensu lato")) %>% 
-  mutate(common_name = str_remove_all(common_name, "[A-Z]{2,}")) 
-
+bird %>% 
+  str_trim(common_name, side = right)
 
 
   
-all_data <- full_join(bird, ship, by = "record_id")
+birds_common_names <- bird %>% 
+  group_by(common_name) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+
+
+  
+# q#all_data <- full_join(bird, ship, by = "record_id")
 
 
 
